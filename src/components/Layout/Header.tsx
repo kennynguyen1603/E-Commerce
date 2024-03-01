b68172388861f1bfb6a7908b60648c34e67f91e0
kennynguyen1603 <kennynguyen1631984@gmail.com>
2024-03-02 03:05:53 +0700

    Update Header and footer

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,7 +1,24 @@
-// import React from 'react'
+import React from 'react';
+import { FaApple } from "react-icons/fa6";
 
+//bg-custom-gradient
 export default function Header() {
   return (
-    <div>Header</div>
-  )
+    <div className="home container mx-auto flex justify-evenly p-5 text-custom-gray items-center font-bold">
+      <div className="logo flex items-center space-x-1.5">
+        <FaApple />
+        <a href='/home'>ex.iphones</a>
+      </div>
+      <div className="routes flex space-x-6">
+        <a href="/home">Home</a>
+        <a href="/contact">Contact</a>
+        <a href="/about">About</a>
+      </div>
+      <div className="auth flex space-x-2.5">
+        <a href="/login">Login</a>
+        <p>|</p>
+        <a href="/signup">Sign up</a>
+      </div>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,18 @@ export default {
     "node_modules/flowbite-react/lib/esm/**/*.js",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "custom-gradient":
+          "linear-gradient(155.28deg, #383638 2.06%, #0B0B0B 33.08%)",
+      },
+      colors: {
+        "custom-gray": "#8B8E99",
+      },
+    },
+    fontFamily: {
+      inter: ["Inter", "sans-serif"],
+    },
   },
   plugins: [import("flowbite/plugin")],
 };
